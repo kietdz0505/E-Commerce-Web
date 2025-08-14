7ee2075af02c25113e95b4435fcce1d2a00b0a45
kietdz0505 <tgkiet03@gmail.com>
2025-08-14 17:37:21 +0700

    fix deploy

--- src/main/java/com/example/ecommerce_web/EcommerceWebApplication.java
+++ src/main/java/com/example/ecommerce_web/EcommerceWebApplication.java
@@ -7,7 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceWebApplication {
 
 	public static void main(String[] args) {
+		// In ra tất cả biến môi trường
+		System.out.println("=== ENVIRONMENT VARIABLES ===");
+		System.getenv().forEach((key, value) -> {
+			if (key.toUpperCase().contains("VNPAY")) { // chỉ in biến liên quan VNPay
+				System.out.println(key + "=" + value);
+			}
+		});
+		System.out.println("==============================");
+
 		SpringApplication.run(EcommerceWebApplication.class, args);
 	}
-
 }
+
+
