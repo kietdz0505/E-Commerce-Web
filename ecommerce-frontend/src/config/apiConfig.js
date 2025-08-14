9f7ba2a2a8d227b130a70055afe630cf1f5469a9
kietdz0505 <tgkiet03@gmail.com>
2025-08-14 19:34:15 +0700

    fix url

--- ecommerce-frontend/src/config/apiConfig.js
+++ ecommerce-frontend/src/config/apiConfig.js
@@ -2,7 +2,7 @@ import { UPDATE } from 'react-admin';
 import PaginationConfig from './paginationConfig';
 
 export const API_CONFIG = {
-  BASE_URL: 'https://e-commerce-web-xm8f.onrender.com/',
+  BASE_URL: 'https://e-commerce-web-xm8f.onrender.com',
 
   OAUTH: {
     FACEBOOK: '/oauth2/authorization/facebook',
