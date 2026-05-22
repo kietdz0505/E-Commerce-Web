feecd29e0127dcd1d275d453a2e0b75bbc994794
kietdz0505 <tgkiet03@gmail.com>
2026-05-22 22:42:12 +0700

    fix timezone

--- ecommerce-backend/src/main/java/com/example/ecommerce_web/EcommerceWebApplication.java
+++ ecommerce-backend/src/main/java/com/example/ecommerce_web/EcommerceWebApplication.java
@@ -19,15 +19,6 @@ public class EcommerceWebApplication {
 	}
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing()
-				.load();
-
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
 		SpringApplication.run(EcommerceWebApplication.class, args);
 	}
 }
