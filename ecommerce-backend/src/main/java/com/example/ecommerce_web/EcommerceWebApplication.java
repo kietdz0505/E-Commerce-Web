c494f7520841db8fe1a9f911992199f5e2f084d1
kietdz0505 <tgkiet03@gmail.com>
2026-05-22 19:26:30 +0700

    fix timezone

--- ecommerce-backend/src/main/java/com/example/ecommerce_web/EcommerceWebApplication.java
+++ ecommerce-backend/src/main/java/com/example/ecommerce_web/EcommerceWebApplication.java
@@ -1,13 +1,22 @@
 package com.example.ecommerce_web;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class EcommerceWebApplication {
+
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 
